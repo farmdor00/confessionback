78a18f6b4aee841c5ea73370a88f52e5c8bf8d74
farmdor00 <farmdor00@gmail.com>
2024-11-26 22:57:54 +0530

    Update Confession.js

--- models/Confession.js
+++ models/Confession.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const confessionSchema = new mongoose.Schema({
     text: { type: String, required: true },
-  },{timestamps: true});
+    createdAt: { type: Date, default: Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }) },
+  });
 const Confession = mongoose.model('Confession', confessionSchema);
 
 module.exports = Confession
